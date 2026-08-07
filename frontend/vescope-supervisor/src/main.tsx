562b18657435ab93e922b9f33ab72635c00e8f90
Cheikh Tidiane DIOP <151855898+Scorpion160@users.noreply.github.com>
2026-08-07 11:24:34 +0000

    Intégrer le basculement clair sombre

--- frontend/vescope-supervisor/src/main.tsx
+++ frontend/vescope-supervisor/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ThemeToggle from './ThemeToggle';
 import './styles.css';
+import './theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ThemeToggle />
   </React.StrictMode>,
 );
