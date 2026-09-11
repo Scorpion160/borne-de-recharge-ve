e547c5a2cb711c84316c3348def2ec87a11e43f4
Cheikh Tidiane DIOP <151855898+Scorpion160@users.noreply.github.com>
2026-09-11 11:58:51 +0000

    Passer le firmware terrain en 0.2.4

--- firmware/vescope-core-esp32/include/config.h
+++ firmware/vescope-core-esp32/include/config.h
@@ -5,7 +5,7 @@
 namespace vescope {
 
 constexpr char DEVICE_ID[] = "borne-01";
-constexpr char FIRMWARE_VERSION[] = "0.2.3-field";
+constexpr char FIRMWARE_VERSION[] = "0.2.4-field";
 
 // -----------------------------------------------------------------------------
 // Cible matérielle
