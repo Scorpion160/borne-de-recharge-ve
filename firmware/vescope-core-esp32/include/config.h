7bfe083e6c92652bfccab8b585a195713e2282f2
Cheikh Tidiane DIOP <151855898+Scorpion160@users.noreply.github.com>
2026-09-14 10:29:39 +0000

    chore(firmware): bump field version for TLS bundle refresh

--- firmware/vescope-core-esp32/include/config.h
+++ firmware/vescope-core-esp32/include/config.h
@@ -5,7 +5,7 @@
 namespace vescope {
 
 constexpr char DEVICE_ID[] = "borne-01";
-constexpr char FIRMWARE_VERSION[] = "0.2.5-field";
+constexpr char FIRMWARE_VERSION[] = "0.2.6-field";
 
 // -----------------------------------------------------------------------------
 // Cible matérielle
