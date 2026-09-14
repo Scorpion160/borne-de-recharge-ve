3c5522c98d35b4bff5ac413b4fe7c1083057b19e
Cheikh Tidiane DIOP <151855898+Scorpion160@users.noreply.github.com>
2026-09-14 12:16:56 +0000

    firmware: reset HTTPS client per publish for reliability

--- firmware/vescope-core-esp32/include/https_fallback.h
+++ firmware/vescope-core-esp32/include/https_fallback.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include <Arduino.h>
-#include <HTTPClient.h>
-#include <WiFiClientSecure.h>
 
 namespace vescope {
 
@@ -16,15 +14,13 @@ class HttpsFallback {
   uint32_t errorCount() const;
 
  private:
-  WiFiClientSecure secure_;
-  HTTPClient http_;
   uint32_t last_attempt_ms_ = 0;
   uint32_t last_success_ms_ = 0;
+  uint32_t last_telemetry_attempt_ms_ = 0;
   uint32_t retry_ms_ = 1000;
   uint32_t success_count_ = 0;
   uint32_t error_count_ = 0;
   int last_http_code_ = 0;
-  bool configured_ = false;
 };
 
 }  // namespace vescope
