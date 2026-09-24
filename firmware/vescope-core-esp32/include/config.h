166a5d1102d6a144dbc9dd724a8cbecc1a2c76aa
Cheikh Tidiane DIOP <151855898+Scorpion160@users.noreply.github.com>
2026-09-24 09:52:40 +0000

    firmware: resilient AP and prioritized SN connection (include/config.h)

--- firmware/vescope-core-esp32/include/config.h
+++ firmware/vescope-core-esp32/include/config.h
@@ -5,7 +5,7 @@
 namespace vescope {
 
 constexpr char DEVICE_ID[] = "borne-01";
-constexpr char FIRMWARE_VERSION[] = "0.2.13-field";
+constexpr char FIRMWARE_VERSION[] = "0.2.14-field";
 
 // -----------------------------------------------------------------------------
 // Cible matérielle
